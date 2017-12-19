efa2e8d0c908fd5f7d4ac712c6541b4386e68975
Carlos Astudillo Jamett <Carlos Astudillo Jamett>
2017-12-19 09:41:12 -0300

    Index Ok + Nuevos Archivos creados(Libro del anio)

---
+++ libro-del-ano-aventureros-2018-cuentame-de-elena-de-white.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>.::Somos Aventureros | Libro del Año 2018 Aventureros - Cuentame de Elena de White::.</title>
+<!--INICIO METAS-->
+<meta charset="utf-8">
+<meta http-equiv="Content-Language" content="es"/>
+<meta name="Keywords" content="Libro del Año 2018 Aventureros - Cuentame de Elena de White"/>
+<meta name="Distribution" content="global"/>
+<meta name="Robots" content="all"/>
+<!--FIN METAS-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/tms-0.4.1.js"></script>
+<script src="js/slider.js"></script>
+<script src="js/custom_recursos.js"></script>
+<!--[if lt IE 9]>
+<script src="../js/html5.js"></script>
+<link rel="stylesheet" href="../css/ie.css"> 
+<![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62506720-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+<!-- Inicio Script Para Me Gusta Facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Fin Script Para Me Gusta Facebook -->
+<div class="main-indents">
+  <div class="main">
+    <!-- Header -->
+    <?php require_once("menu.php"); ?>
+    <!-- Content -->
+    <section id="content">
+      <div class="content-child-pages">
+
+        <h5>LIBRO DEL AÑO 2018 -AVENTUREROS - 2018</h5>
+        <p><img class="img-box" src="images/recursos/cuentame.png" /></p>
+        <p>David es un niño como tu, que todo lo pregunta. En dialogo con su mamá, quiere aprender acerca de Elena de White. Descubre la vida de Elena de White, miestras imaginas el próposito que Dios tenía para ella.
+        </p>
+        <p>Queremos invitarlos a Adquirirlo, tiene un costo en Chile de $3.920</p>
+        <p>
+          <h4><a target="_blank" href="http://www.aceschile.cl/libro/Cuentame_de_Elena_de_White_-_Ilustrado-9023/"><strong>Mas información</strong></a></h4>
+        </p>
+        <!--Inicio Like Facebook-->
+        <div class="fb-like" data-href="libro-del-ano-aventureros-2018-cuentame-de-elena-de-white.php" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+        <!--Fin Like Facebook-->
+        <!--Inicio Comentarios Facebook-->        
+        <div class="fb-comments" data-href="libro-del-ano-aventureros-2018-cuentame-de-elena-de-white.php" data-numposts="100"></div>
+        <!--Inicio Comentarios Facebook-->  
+        </p>  
+      </div>
+    </section>
+    <!-- Footer -->
+    <?php require_once("footer.php"); ?>
+  </div>
+</div>
+</body>
+</html>
