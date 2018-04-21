e9efc40721219fa3e69089d9d7c19b0cd158fb51
Administrator <gitlab@raef.cl>
2018-04-21 12:50:32 -0300

    nuevas imagenes y archivos RFA

---
+++ recursos-para-el-club-de-aventureros-red-familiar.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>.::Somos Aventureros | Recursos para el club de aventureros - Red Familar::.</title>
+<!--INICIO METAS-->
+<meta charset="utf-8">
+<meta http-equiv="Content-Language" content="es"/>
+<meta name="Keywords" content="RFA, Red, Familiar, Aventureros"/>
+<meta name="Description" content="Club de Aventureros, 
+Recursos para el club de aventureros,Recursos para lideres de 
+aventureros, Como trabajar cn la red familiar, red familiar de aventureros, RFA"/>
+<meta name="Distribution" content="global"/>
+<meta name="Robots" content="all"/>
+<!--FIN METAS-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/tms-0.4.1.js"></script>
+<script src="js/slider.js"></script>
+<script src="js/custom_recursos.js"></script>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<link rel="stylesheet" href="css/ie.css"> 
+<![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62506720-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+<!-- Inicio Script Para Me Gusta Facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Fin Script Para Me Gusta Facebook -->
+<div class="main-indents">
+  <div class="main">
+    <!-- Header -->
+    <?php require_once("menu.php"); ?>
+    <!-- Content -->
+    <section id="content">
+      <div class="content-child-pages">
+
+        <h5>RECURSOS PARA EL CLUB DE AVENTUREROS - RED FAMILIAR</h5>
+            <div class="container">
+            <div class="container" id="contenidos">
+            <!--INICIO CONTENIDOS -->
+            <!--inicio recurso -->
+            <div class="recursos">
+                <table width="75%">
+                <tr>
+                    <td rowspan="2">
+                      <img height="160px" width="160px" src="images/recursos/rfa-01.jpg" class="img-box" alt="">
+                    </td>
+                    <td>
+                      <div class="titulo-recursos">
+                        <h6>GUIA OFICIAL PARA TRABAJAR CON LA RED FAMILIAR DE AVENTUREROS</h6>
+                      </div>    
+                    </td>
+                </tr>          
+                <tr>
+                <td><div class="contenido-recursos">
+                La RFA es uno de los programas mas nuevos del club de aventureros en el sentido formal, ya que siempre el club de aventureros ha sido un ministerio familiar, sin embargo en los ultimos años la DSA se ha preocupado de proveer abundante material y guias para que hagamos un trabajo de acuerdo al deseo del Señor. El siguiente documento es un extracto del Manual Administrativo del Club de Aventureros y es una lectura obligatoria para cualquier persona que este interesada en iniciar el trabajo de la Red Familar en su club. 
+                <a target="_blank" href="https://drive.google.com/open?id=15HA56tppvkqBVvYHeSiKZ_HFUNMOxE1j"><b>Descargar</b></a> 
+                 </div>
+                 </td>
+                 </tr>          
+                </table>
+            </div>  
+            <!--fin recurso -->
+            <br>                      
+            <!--inicio recurso -->
+            <div class="recursos">
+                <table width="75%">
+                <tr>
+                    <td rowspan="2">
+                      <img height="160px" width="160px" src="images/recursos/rfa-02.jpg" class="img-box" alt="">
+                    </td>
+                    <td>
+                      <div class="titulo-recursos">
+                        <h6>IDEAS PRACTICAS PARA EL TRABAJO CON LA RED FAMILIAR DE AVENTUREROS</h6>
+                      </div>    
+                    </td>
+                </tr>          
+                <tr>
+                <td><div class="contenido-recursos">
+                La siguiente presentación tiene 70 diapostivas con ideas practicas y concretas para trabajar con la Red Familiar de tu club. Es un excelente material desarrollado por la GM Yanina Melis, lider de amplia experiencia en la materia.
+                <a target="_blank" href="http://www.adventistas.org/es/ninos/proyecto/guardianes-los-tesoros/"><b>Descargar</b></a> 
+                 </div>
+                 </td>
+                 </tr>          
+                </table>
+            </div>  
+            <!--fin recurso -->
+            <br>
+            <!--inicio recurso -->
+            <div class="recursos">
+                <table width="75%">
+                <tr>
+                    <td rowspan="2">
+                      <img height="160px" width="160px" src="images/recursos/rfa2.png" class="img-box" alt="">
+                    </td>
+                    <td>
+                      <div class="titulo-recursos">
+                        <h6>COMO RELACIONARNOS CON LOS PADRES DE LOS AVENTUREROS</h6>
+                      </div>    
+                    </td>
+                </tr>          
+                <tr>
+                <td><div class="contenido-recursos">Cuando uno piensa en describir un club de aventureros dice algo como "Es un club para niños de 3(para los que tienen castorcitos) a 9 años"
+                que les ayuda a desarrollarse de manera integral en las areas espiritual, social y física ... <a href="como-relacionarnos-con-los-padres-de-los-aventureros.php"><b>Ver Mas</b></a> 
+                 </div>
+                 </td>
+                 </tr>          
+                </table>
+            </div>  
+            <!--fin recurso -->
+            <!--FIN CONTENIDOS -->
+              <!--Inicio Like Facebook-->
+              <div class="fb-like" data-href="http://www.somosaventureros.cl/recursos-para-el-club-de-aventureros-red-familiar.php" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+              <!--Fin Like Facebook-->
+            </div>    
+      </div>
+    </section>
+    <!-- Footer -->
+    <?php require_once("footer.php"); ?>
+  </div>
+</div>
+</body>
+</html>
