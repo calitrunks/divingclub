55f188b5d95b8c89dc6f417452fb09e4ec7036a5
Administrator <gitlab@raef.cl>
2018-06-07 17:25:04 -0400

    guia de especialidades al 70%

---
+++ como-organizar-una-feria-de-especialidades-de-aventureros-en-7-pasos.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>.::Somos Aventureros | Como Organizar una Feria de Especialidades de Aventureros en 7 pasos.::.</title>
+<!--INICIO METAS-->
+<meta charset="utf-8">
+<meta http-equiv="Content-Language" content="es"/>
+<meta name="Keywords" content="Especialidades, Aventureros, Feria, Guia, Parches"/>
+<meta name="Description" content="Feria de Especialidades, Como Organizar una Feria de Especialidades de Aventureros, Guia para Feria de Especialidades"/>
+<meta name="Distribution" content="global"/>
+<meta name="Robots" content="all"/>
+<!--FIN METAS-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/tms-0.4.1.js"></script>
+<script src="js/slider.js"></script>
+<script src="js/custom_recursos.js"></script>
+<!--[if lt IE 9]>
+<script src="../js/html5.js"></script>
+<link rel="stylesheet" href="../css/ie.css"> 
+<![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62506720-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+<!-- Inicio Script Para Me Gusta Facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Fin Script Para Me Gusta Facebook -->
+<div class="main-indents">
+  <div class="main">
+    <!-- Header -->
+    <?php require_once("menu.php"); ?>
+    <!-- Content -->
+    <section id="content">
+      <div class="content-child-pages">
+
+        <h5><b>Como Organizar una Feria de Especialidades de Aventureros en 7 pasos</b></h5>
+        <p><img class="img-box" src="images/recursos/disciplina.png" /></p>
+        <p class="contenido-recursos">
+         El Manual de Aventureros para la división sudamericana establece un total de 53 Especialidades, <a href="https://www.adventurer-club.org/adventurer-awards/overview" target="_blank">si a eso le sumamos las especialidades publicadas en ingles por el ministerio joven de la asociación general</a> el numero supera facilmente las 100. 
+        </p>
+        <p class="contenido-recursos">
+          En mi experiencia, un aventurero que haya estado 4 años en el club puede alcanzar unas 30 especialidades en el mejor de los casos, la mayoria de ellas las soclicitadas por las clases progresivas y ¿Que hay del resto?, las ferias de especialidades son una buena oportunidad para alcanzar nuevos honores.
+        </p>
+        <p class="contenido-recursos">
+          <span><b>¿Que es una feria de especialidades?</b></span><br>
+          Es un programa que generalmente tiene un día de duración en donde el enfasis principal esta en que los niños puedan alcanzar nuevas especialidades, es decir, se enfoca principalmente en que los niños reciban instrucción sobre temas específicos, puede ser organizada por un club para sus integrantes o incluso que ese club invite a otros clubes tambien a participar, tambien es posible que el Distrito, Zona o Asociación o Misión preparen el programa. ¿Te gustaría saber como organizar una feria?,  sigue leyendo porque ahora vamos con una guía paso a paso para conseguirlo.
+        </p>
+        <p class="contenido-recursos">
+          <span><b>1.- Planificar el Evento</b></span><br>
+          Como cualquier actividad que el ser humano realiza requiere tiempo de sentarse a conversar y definir los objetivos que se desean alcanzar, las ideas en el aire no suelen dar muy buenos resultados,  asi que toma tu computador o simplemente papel y lapiz, reune a tu equipo de trabajo y comienza a definir las ideas de tu programa. Aqui algunas cosas que debería incluir tu planificación.
+          <ul class="contenido-recursos">
+            <li>a) Objetivos(que esperamos alcanzar con el programa, en otras palabras porque hacemos la feria)</li>
+            <li>b)Fecha</li>
+            <li>c)Duracion del programa</li>
+            <li>d)Lugar en que se realizará el evento</li>
+            <li>e)Quienes pueden participar(tu club, otros clubes, etc)</li>
+            <li>f)Cuantas y cuales especialidades se dictaran(idealmente deben ser especialidades que los niños no harán como parte de las clases progresivas)</li>
+            <li>g)Cuantos cupos habrá por cada especialidad(idealmente no deberian haber mas de 12 niños por especialidad)</li>
+            <li>h)Quienes serán los instructores</li>
+            <li>i)Cual será el presupuesto(considerar arriendo del lugar, parches para los niños, certificados, alimentación, escenografia, etc)</li>
+            <li>j)Con el presupuesto hecho definir el valor de inscripción</li>
+            <li>Forma de Inscripción y plazos de inscripción(en persona, via internet, etc)</li>
+            <li>k)Definir las comisiones de trabajo y responsables de cada una(promoción, escenografía, conseguir los instructores, alimentacion, inscripciones, compra de materiales, etc) </li>
+          </ul>
+        </p>
+        <p class="contenido-recursos">
+          <span><b>2.- Formalizando con la Junta de Iglesia</b></span><br>
+          Antes de siquiera contarle a alguien sobre esta idea debes validar tu proyecto con la junta de tu iglesia, tener el apoyo y respaldo de los lideres de tu iglesia no solo es lo correcto, es lo mejor para garantizar el exito de tu evento. Ya se que muchas veces los hermanos no entienden las locas ideas de los lideres de aventureros, pero tu haz lo correcto que Dios bendecirá tus esfuerzo, busca apoyo, involucra a tu pastor con alguna meditación, pide apoyo al departamento de comunicaciones para saber como difundir la actividad, no actues como una isla, el club no lo es, somos parte de un cuerpo, la iglesia apreciará ver un plan de trabajo tan ordenado y puede que si lo necesitas hasta te apoyen con recuersos financieros para hacer que la actividad sea de bendición para todos.
+        </p>
+        <p class="contenido-recursos">
+          <span><b>3.- Inicio del Trabajo</b></span><br>
+          Ahora que ya esta todo en orden, es el momento de comenzar el trabajo. Cada persona responsable de as comisiones deberá ponerse a trabajar en sus responsabilidades, es muy importante que cada uno cumpla con
+          las labores para asi garantizar el exito de la actividad. Mientras el equipo de publicidad promociona el evento, ya el encargado de coordinar con los instructores debe estar viendo todos los detalles, de la misma forma cada persona del equipo cumpliendo su labor.
+        </p>
+        <p class="contenido-recursos">
+          <span><b>4.- Revisión de Avance</b></span><br>
+          Es bueno realizar periodicamente revisiones de avance de cada una de las comisiones, ante cualquier eventualidad los encargados deberian informar al director las situaciones que no puedan resolver, para asi
+          nada impida el buen funcionamiento del proyecto. Te sugiero que hagas al menos una reunión 1 mes antes y luego una semana antes del evento para ir haciendo seguimiento de los detalles.
+        </p>
+        <p class="contenido-recursos">
+          <span><b>5.- Cierre de Inscrpiciones</b></span><br>
+        El cierre de inscripciones debería ocurrir como mínimo 15 días antes de la fecha de la feria, ¿porque?, pues porque tu deberas preparar los materiales, los parches, almuerzos y muchos otros detalles y para eso debes saber realmente cuantas personas van a participar, es por eso que te recomiendo que las inscripciones sean siempre con el pago de la inscripción o al menos el 60% del valor total, aunque como las ferias son eventos que no tienen inscripcions muy caras lo mejor sería que pagaran los asistentes pagaran la inscripción completa de una vez, pues en caso de que las personas no asistan a ultima hora eso puede afectar tu presupuesto, esto no se trata necesariamente de reunir dinero para el club, pero tampoco perderlo.
+        </p>
+        <p class="contenido-recursos">
+          <span><b>6.- El día Esperado</b></span><br>
+        
+        </p>
+        <p class="contenido-recursos">
+          <span><b>7.- Evaluación</b></span><br>
+        
+        </p>
+        <p class="contenido-recursos">
+          <span><b>CONCLUSIONES</b></span><br>
+        
+        </p>
+
+        
+        
+        <!--Inicio Like Facebook-->
+        <div class="fb-like" data-href=como-organizar-una-feria-de-especialidades-de-aventureros-en-7-pasos.php" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+        <!--Fin Like Facebook-->
+        <!--Inicio Comentarios Facebook-->        
+        <div class="fb-comments" data-href="como-organizar-una-feria-de-especialidades-de-aventureros-en-7-pasos.php" data-numposts="100"></div>
+        <!--Inicio Comentarios Facebook-->  
+        </p>  
+      </div>
+    </section>
+    <!-- Footer -->
+    <?php require_once("footer.php"); ?>
+  </div>
+</div>
+</body>
+</html>
---
+++ recursos-para-el-club-de-aventureros-guia-para-trabajar-con-clases-progresivas.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>.::Somos Aventureros | Como trabajar con las clases progresivas del Club de Aventureros::.</title>
+<!--INICIO METAS-->
+<meta charset="utf-8">
+<meta http-equiv="Content-Language" content="es"/>
+<meta name="Keywords" content="Consejeros, Clases, Guia, Manial"/>
+<meta name="Description" content="Club de Aventureros, 
+Recursos para el club de aventureros,Recursos para lideres de 
+aventureros, Clases progresivas, Guia para consejeros de aventureros, manual para consejeros de aventureros, clases progresivas"/>
+<meta name="Distribution" content="global"/>
+<meta name="Robots" content="all"/>
+<!--FIN METAS-->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/jquery-1.7.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/tms-0.4.1.js"></script>
+<script src="js/slider.js"></script>
+<script src="js/custom_recursos.js"></script>
+<!--[if lt IE 9]>
+<script src="../js/html5.js"></script>
+<link rel="stylesheet" href="../css/ie.css"> 
+<![endif]-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62506720-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+<!-- Inicio Script Para Me Gusta Facebook -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<!-- Fin Script Para Me Gusta Facebook -->
+<div class="main-indents">
+  <div class="main">
+    <!-- Header -->
+    <?php require_once("menu.php"); ?>
+    <!-- Content -->
+    <section id="content">
+      <div class="content-child-pages">
+
+        <h5>GUIA OFICIAL PARA TRABAJO CON CLASES PROGRESIVAS</h5>
+        <p><figure class="img-box"> <img src="images/recursos/guia-clases.jpg" alt=""> </figure></p>
+        <p>
+          Me nombraron consejero del club, y ahora ¿Como tomo la clase?. Es curioso que siendo los consejeros los que se encargan de desarrollar el curriculo del club con los aventureros, esta muchas veces sea tomada como una responsabilidad de menos importancia, muy pocas veces reciben el entrenamiento correspondiente y el desarrollo de la clase muchas veces queda entregado al criterio de cada uno. Gracias a Dios su iglesia ha desarrollado contenidos para guiar este trabajo, el siguiente documento es un extracto del manual administrativo de aventureros(Cap 8), que detalla paso a paso como se debe desarrollar una clase progresiva. ¿Te piden hacer una linea de tiempo?, ¿Jugar al juego de los sentimientos?, eso y todo lo que necesitas esta detallado en este documento. Ya lo sabes, si eres consejero, dale clic a descargar y busca la dirección del Señor para empezar a trabajar como es el plan de Dios para tu ministerio.
+        </p>
+        <p>Pueden descargarlo desde el siguiente <a target="_blank" href="https://drive.google.com/file/d/17LF7tBgao7u6Oeu6q2GKAuQn__akH2WI/view?usp=sharing"><b>link</b></a></p>
+        <!--Inicio Like Facebook-->
+        <div class="fb-like" data-href="http://www.somosaventureros.cl/recursos-para-el-club-de-aventureros-guia-para-trabajar-con-clases-progresivas.php" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
+        <!--Fin Like Facebook-->
+        </p>  
+      </div>
+    </section>
+    <!-- Footer -->
+    <?php require_once("footer.php"); ?>
+  </div>
+</div>
+</body>
+</html>
